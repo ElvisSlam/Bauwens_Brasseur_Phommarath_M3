d341e3177d45582f91de4e3d1825b7bc0add1cfc
Braxasss <antbrasseur1108@gmail.com>
2022-03-21 17:09:50 +0100

    Debut affichage biens, modif css, modif piedDePage

--- vuescontroleurs/listeBiens.php
+++ vuescontroleurs/listeBiens.php
@@ -1,21 +1,23 @@
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title>France Immobilier : Affichage</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
-</head>
-<body>
-  ...
-  <!-- Le reste du contenu -->
-  ...
-</body>
+<html>
+    <?php
+    include_once'../inc/entete.inc'
+    ?>
+    <link rel="stylesheet" href="css\cssimmo.css"/>
+    <body>
+        <table>
+            <tr>
+                <th>Reference</th>
+                <th>Ville</th>
+                <th>Type</th>
+                <th>Prix</th>
+            </tr>
+        <?php
+            include_once '../modeles/mesFonctionsAccesBDD.php';
+            $lePdo=connexionBDD();
+        ?>
+        </table>
+        <?php
+        include_once '../inc/piedDePage.inc';
+        ?>
+    </body>
 </html>
-<link rel="stylesheet" href="../css/cssimmo.css"/>
-<?php
-// put your code here
-include_once '../modeles/mesFonctionsAccesBDD.php';
-$lePdo = connexionBDD();
-include_once'../inc/entete.inc'
-?>
