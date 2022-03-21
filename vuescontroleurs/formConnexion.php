44320333b7588eba07e302552f0e1a2a885c6d77
ElvisSlam <elvisbauwens@gmail.com>
2022-03-21 17:09:24 +0100

    legere modification

--- vuescontroleurs/formConnexion.php
+++ vuescontroleurs/formConnexion.php
@@ -20,7 +20,7 @@
                 <label><b>Mot de passe</b></label>
                 <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-                <input type="submit" id='submit' value='LOGIN' >
+                <input type="submit" id='submit' value='se connecter' >
                 <?php
                 if(isset($_GET['erreur'])){
                     $err = $_GET['erreur'];
