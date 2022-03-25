a4826fc08f03e83113bb360fbb6ee4220dc4a053
Braxasss <antbrasseur1108@gmail.com>
2022-03-25 11:28:23 +0100

    correction html liste biens

--- vuescontroleurs/listeBiens.php
+++ vuescontroleurs/listeBiens.php
@@ -1,27 +1,24 @@
-<html>
+<?php
+include_once'../inc/entete.inc'
+?>
+<table>
+    <tr>
+        <th>Reference</th>
+        <th>Ville</th>
+        <th>Type</th>
+        <th>Prix</th>
+    </tr>
     <?php
-    include_once'../inc/entete.inc'
+    include_once '../modeles/mesFonctionsAccesBDD.php';
+    $lePdo = connexionBDD();
+    $lesBiens = getLesBiens($lePdo);
+    foreach ($lesBiens as $unBien) {
+        echo '<tr> <td>' . $unBien['reference'] . '</td><td>' . $unBien['ville'] . '</td><td>' . $unBien['type'] . '</td><td>' . $unBien['prix'] . '</td></tr>';
+    }
     ?>
-    <link rel="stylesheet" href="css\cssimmo.css"/>
-    <body>
-        <table>
-            <tr>
-                <th>Reference</th>
-                <th>Ville</th>
-                <th>Type</th>
-                <th>Prix</th>
-            </tr>
-            <?php
-            include_once '../modeles/mesFonctionsAccesBDD.php';
-            $lePdo = connexionBDD();
-            $lesBiens = getLesBiens($lePdo);
-            foreach ($lesBiens as $unBien) {
-                echo '<tr> <td>'.$unBien['reference'].'</td><td>'.$unBien['ville'].'</td><td>'.$unBien['type'].'</td><td>'.$unBien['prix'].'</td></tr>';
-            }
-            ?>
-        </table>
-        <?php
-        include_once '../inc/piedDePage.inc';
-        ?>
-    </body>
+</table>
+<?php
+include_once '../inc/piedDePage.inc';
+?>
+</body>
 </html>
