57a71e666f78b4d2a4ad67d3593ce39efab23409
Braxasss <antbrasseur1108@gmail.com>
2022-04-25 00:03:25 +0200

    Commencement PDF

--- modeles/mesFonctionsAccesBDD.php
+++ modeles/mesFonctionsAccesBDD.php
@@ -31,7 +31,8 @@ function testlogin($pdo, $username, $password) {
 
     return $logok;
 }
-function AjoutBien($pdo, $reference, $ville, $type, $prix ,$description, $surface, $nbpiece, $jardin) {
+
+function AjoutBien($pdo, $reference, $ville, $type, $prix, $description, $surface, $nbpiece, $jardin) {
     $requete = $pdo->prepare("INSERT INTO biens VALUES (:reference,:ville,:type,:prix,:description,:surface,:nbpiece,:jardin)");
     $bv1 = $requete->bindValue(':reference', $reference, PDO::PARAM_STR);
     $bv2 = $requete->bindValue(':ville', $ville, PDO::PARAM_STR);
@@ -45,65 +46,65 @@ function AjoutBien($pdo, $reference, $ville, $type, $prix ,$description, $surfac
     return $exec;
 }
 
-
-function getLesBiens($pdo, $ville, $type, $jardin, $prixmin, $prixmax){
-    $pdostatement=$pdo->prepare("SELECT reference,ville,type,prix,jardin FROM biens WHERE ville LIKE :rechVille AND type LIKE :rechType AND jardin LIKE :rechJardin AND prix BETWEEN :rechPrixmin AND :rechPrixmax");
-    $bv1=$pdostatement->bindValue(':rechVille',$ville, PDO::PARAM_STR);
-    $bv2=$pdostatement->bindValue(':rechType',$type, PDO::PARAM_STR);
-    $bv3=$pdostatement->bindValue(':rechJardin',$jardin, PDO::PARAM_STR);
-    $bv4=$pdostatement->bindValue(':rechPrixmin',$prixmin, PDO::PARAM_INT);
-    $bv5=$pdostatement->bindValue(':rechPrixmax',$prixmax, PDO::PARAM_INT);
-    $exec=$pdostatement->execute();
-    $resultat=$pdostatement->fetchAll();
-    return $resultat;
-}
- 
-
-function getLesVilles($pdo){
-    $pdostatement=$pdo->prepare("SELECT DISTINCT ville FROM biens");
-    $exec=$pdostatement->execute();
-    $resultat=$pdostatement->fetchAll();
+function getLesBiens($pdo, $ville, $type, $jardin, $prixmin, $prixmax) {
+    $pdostatement = $pdo->prepare("SELECT reference,ville,type,prix,jardin FROM biens WHERE ville LIKE :rechVille AND type LIKE :rechType AND jardin LIKE :rechJardin AND prix BETWEEN :rechPrixmin AND :rechPrixmax");
+    $bv1 = $pdostatement->bindValue(':rechVille', $ville, PDO::PARAM_STR);
+    $bv2 = $pdostatement->bindValue(':rechType', $type, PDO::PARAM_STR);
+    $bv3 = $pdostatement->bindValue(':rechJardin', $jardin, PDO::PARAM_STR);
+    $bv4 = $pdostatement->bindValue(':rechPrixmin', $prixmin, PDO::PARAM_INT);
+    $bv5 = $pdostatement->bindValue(':rechPrixmax', $prixmax, PDO::PARAM_INT);
+    $exec = $pdostatement->execute();
+    $resultat = $pdostatement->fetchAll();
     return $resultat;
 }
 
-function getLesTypes($pdo){
-    $pdostatement=$pdo->prepare("SELECT type FROM type");
-    $exec=$pdostatement->execute();
-    $resultat=$pdostatement->fetchAll();
+function getLesVilles($pdo) {
+    $pdostatement = $pdo->prepare("SELECT DISTINCT ville FROM biens");
+    $exec = $pdostatement->execute();
+    $resultat = $pdostatement->fetchAll();
     return $resultat;
 }
 
-function getPrixMax($pdo){
-    $pdostatement=$pdo->prepare("SELECT MAX(prix) FROM biens");
-    $exec=$pdostatement->execute();
-    $resultat=$pdostatement->fetch();
+function getLesTypes($pdo) {
+    $pdostatement = $pdo->prepare("SELECT type FROM type");
+    $exec = $pdostatement->execute();
+    $resultat = $pdostatement->fetchAll();
+    return $resultat;
+}
+
+function getPrixMax($pdo) {
+    $pdostatement = $pdo->prepare("SELECT MAX(prix) FROM biens");
+    $exec = $pdostatement->execute();
+    $resultat = $pdostatement->fetch();
     $resultatint = intval($resultat[0]);
     return $resultatint;
 }
 
-function getUnBiens($pdo, $reference){
-    $pdostatement=$pdo->prepare("SELECT * FROM biens WHERE reference = :reference");
-    $bv1=$pdostatement->bindValue(':reference',$reference, PDO::PARAM_STR);
-    $exec=$pdostatement->execute();
-    $resultat=$pdostatement->fetch();
+function getUnBiens($pdo, $reference) {
+    $pdostatement = $pdo->prepare("SELECT * FROM biens WHERE reference = :reference");
+    $bv1 = $pdostatement->bindValue(':reference', $reference, PDO::PARAM_STR);
+    $exec = $pdostatement->execute();
+    $resultat = $pdostatement->fetch();
     return $resultat;
 }
+
 function getimage($pdo, $reference) {
-    $pdostatement=$pdo->prepare("SELECT chemin FROM image WHERE reference = :reference");
-    $bv1=$pdostatement->bindValue(':reference',$reference, PDO::PARAM_STR);
-    $exec=$pdostatement->execute();
-    $resultat=$pdostatement->fetch();
+    $pdostatement = $pdo->prepare("SELECT chemin FROM image WHERE reference = :reference");
+    $bv1 = $pdostatement->bindValue(':reference', $reference, PDO::PARAM_STR);
+    $exec = $pdostatement->execute();
+    $resultat = $pdostatement->fetch();
     return $resultat;
 }
-function getPrixMin($pdo){
-    $pdostatement=$pdo->prepare("SELECT MIN(prix) FROM biens");
-    $exec=$pdostatement->execute();
-    $resultat=$pdostatement->fetch();
+
+function getPrixMin($pdo) {
+    $pdostatement = $pdo->prepare("SELECT MIN(prix) FROM biens");
+    $exec = $pdostatement->execute();
+    $resultat = $pdostatement->fetch();
     $resultatint = intval($resultat[0]);
     return $resultatint;
 }
 
-function ModifBien($pdo, $reference, $ville, $type, $prix ,$description, $surface, $nbpiece, $jardin) {
+function ModifBien($pdo, $reference, $ville, $type, $prix, $description, $surface, $nbpiece, $jardin) {
     $requete = $pdo->prepare("UPDATE biens SET reference=:modifreference, ville=:modifville, type=:modiftype, prix=:modifprix, description=:modifdescription, surface=:modifsurface, nbpiece=:modifnbpiece, jardin=:modifjardin WHERE reference=:modifreference");
     $bv1 = $requete->bindValue(':modifreference', $reference, PDO::PARAM_INT);
     $bv2 = $requete->bindValue(':modifville', $ville, PDO::PARAM_STR);
@@ -116,4 +117,3 @@ function ModifBien($pdo, $reference, $ville, $type, $prix ,$description, $surfac
     $exec = $requete->execute();
     return $exec;
 }
-
--- vuescontroleurs/descriptionbien.php
+++ vuescontroleurs/descriptionbien.php
@@ -41,18 +41,54 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                 <?php echo $unbien['description']; ?>
             </div>
         </div>
-        <button onclick="enregistrerBien()">Télécharger le bien en PDF</button>
-        <script src="../js/html2pdf.bundle.min.js"></script>
+        <button onclick="Convert_HTML_To_PDF();">Convert HTML to PDF</button>
+        <script src="../js/html2canvas.min.js"></script>
+        <script src="../js/jspdf.min.js"></script>
         <script>
-            var descrip = document.getElementById('descri');
-            var descrip_img = document.getElementById('img_bien');
-            var opt = {
-                margin: 1,
-                filename: 'description_bien.pdf',
-                image: { type: 'jpeg', quality: 0.9}
-            };
-            
-            html2pdf().set(opt).from(descrip_img).save();
+            function Convert_HTML_To_PDF() {
+                var elementHTML = document.getElementById('descri');
+
+                html2canvas(elementHTML, {
+                    useCORS: true,
+                    onrendered: function (canvas) {
+                        var pdf = new jsPDF('p', 'pt', 'letter');
+
+                        var pageHeight = 980;
+                        var pageWidth = 900;
+                        for (var i = 0; i <= elementHTML.clientHeight / pageHeight; i++) {
+                            var srcImg = canvas;
+                            var sX = 0;
+                            var sY = pageHeight * i; // start 1 pageHeight down for every new page
+                            var sWidth = pageWidth;
+                            var sHeight = pageHeight;
+                            var dX = 0;
+                            var dY = 0;
+                            var dWidth = pageWidth;
+                            var dHeight = pageHeight;
+
+                            window.onePageCanvas = document.createElement("canvas");
+                            onePageCanvas.setAttribute('width', pageWidth);
+                            onePageCanvas.setAttribute('height', pageHeight);
+                            var ctx = onePageCanvas.getContext('2d');
+                            ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
+
+                            var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+                            var width = onePageCanvas.width;
+                            var height = onePageCanvas.clientHeight;
+
+                            if (i > 0) // if we're on anything other than the first page, add another page
+                                pdf.addPage(612, 864); // 8.5" x 12" in pts (inches*72)
+
+                            pdf.setPage(i + 1); // now we declare that we're working on that page
+                            pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width * .62), (height * .62)); // add content to the page
+                        }
+
+                        // Save the PDF
+                        pdf.save('document.pdf');
+                    }
+                });
+            }
+
         </script>
     </body>
 </html>
