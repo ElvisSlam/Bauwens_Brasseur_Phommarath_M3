6aa618b5b7c83b9fff0b6d1b70f9c3ddf6ef2308
Braxasss <antbrasseur1108@gmail.com>
2022-04-25 15:16:50 +0200

    conversion PDF oskour je suis en état de déficience mental

--- vuescontroleurs/descriptionbien.php
+++ vuescontroleurs/descriptionbien.php
@@ -41,54 +41,48 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                 <?php echo $unbien['description']; ?>
             </div>
         </div>
-        <button onclick="Convert_HTML_To_PDF();">Convert HTML to PDF</button>
+        <button onclick="genererBien();">Convert HTML to PDF</button>
         <script src="../js/html2canvas.min.js"></script>
         <script src="../js/jspdf.min.js"></script>
         <script>
-            function Convert_HTML_To_PDF() {
+            function genererBien() {
+                //effectivement ce code vient d'Internet
                 var elementHTML = document.getElementById('descri');
-
-                html2canvas(elementHTML, {
+                var imgg = document.getElementById('img_bien');
+                html2canvas(imgg, {
                     useCORS: true,
                     onrendered: function (canvas) {
                         var pdf = new jsPDF('p', 'pt', 'letter');
-
                         var pageHeight = 980;
                         var pageWidth = 900;
                         for (var i = 0; i <= elementHTML.clientHeight / pageHeight; i++) {
                             var srcImg = canvas;
                             var sX = 0;
-                            var sY = pageHeight * i; // start 1 pageHeight down for every new page
+                            var sY = pageHeight * i;
                             var sWidth = pageWidth;
                             var sHeight = pageHeight;
                             var dX = 0;
                             var dY = 0;
                             var dWidth = pageWidth;
                             var dHeight = pageHeight;
-
                             window.onePageCanvas = document.createElement("canvas");
                             onePageCanvas.setAttribute('width', pageWidth);
                             onePageCanvas.setAttribute('height', pageHeight);
                             var ctx = onePageCanvas.getContext('2d');
                             ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
-
-                            var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+                            var canvasDataURL = onePageCanvas.toDataURL("image/jpeg", 0.90);
                             var width = onePageCanvas.width;
                             var height = onePageCanvas.clientHeight;
-
-                            if (i > 0) // if we're on anything other than the first page, add another page
-                                pdf.addPage(612, 864); // 8.5" x 12" in pts (inches*72)
-
-                            pdf.setPage(i + 1); // now we declare that we're working on that page
-                            pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width * .62), (height * .62)); // add content to the page
+                            if (i > 0)
+                                pdf.addPage(612, 864);
+                            pdf.setPage(i + 1);
+                            pdf.addImage(canvasDataURL, 'JPEG', 50, 10);
+                            console.log(canvasDataURL);
                         }
-
-                        // Save the PDF
-                        pdf.save('document.pdf');
+                        pdf.save('document_bien.pdf');
                     }
                 });
             }
-
         </script>
     </body>
 </html>
