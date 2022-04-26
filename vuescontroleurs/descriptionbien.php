1a0a405d26768872dbd1440725ed5ea02eefd682
Braxasss <antbrasseur1108@gmail.com>
2022-04-26 12:15:03 +0200

    Generation PDF fonctionnelle

--- vuescontroleurs/descriptionbien.php
+++ vuescontroleurs/descriptionbien.php
@@ -12,6 +12,21 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <link rel="stylesheet" href="../css/cssimmo.css" />
         <meta charset="UTF-8">
         <title></title>
+        <script type="text/javascript" src="../js/html2canvas.min.js"></script>
+        <script type="text/javascript" src="../js/jspdf.min.js"></script>
+        <script type="text/javascript">
+            function genPDF() {
+                html2canvas(document.getElementById("description"), {
+                   onrendered: function(canvas) {
+                       
+                       var img = canvas.toDataURL("image/jpeg");
+                       var doc = new jsPDF();
+                       doc.addImage(img, 'JPEG',20,20);
+                       doc.save('test.pdf');
+                   } 
+                });
+            }
+        </script>
     </head>
     <body>
         <?php
@@ -21,68 +36,22 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         $unbien = getUnBiens($lePdo, $ref);
         $uneimage = getimage($lePdo, $ref)
         ?>
-        <div class="content">
-            <h2 class="title_bien"><?php echo ' ' . $unbien['type'] . ' T' . $unbien['nbpiece'] . ' ' . $unbien['ville']; ?></h2>
-
-            <script src="../slider.js"></script>
-            <div id="slider">
-                <img src="<?php echo $uneimage['chemin']; ?>" class="img_bien" alt="Image du bien immobilier" id="img_bien">
-            </div>
-            <div class="desc" id="descri">
-                <h3><?php echo 'Prix : ' . $unbien['prix'] . ' €'; ?></h3>
-                <p>Caractéristique du bien :</p>
-                <dl>
-                    <dt>Superficie :</dt>
-                    <dd><?php echo $unbien['surface'] . ' m²'; ?></dd>
-                    <dt>Nombre de pièces :</dt>
-                    <dd><?php echo $unbien['nbpiece']; ?></dd>
-                </dl>
-                <h3>Description :</h3>
-                <?php echo $unbien['description']; ?>
-            </div>
+        <div class="content" id="description">
+            <h2><?php echo ' ' . $unbien['type'] . ' T' . $unbien['nbpiece'] . ' ' . $unbien['ville']; ?></h2>
+            <img src="<?php echo $uneimage['chemin']; ?>" class="img_bien" alt="Image du bien immobilier" id="img_bien">
+            <h3><?php echo 'Prix : ' . $unbien['prix'] . ' €'; ?></h3>
+            <p>Caractéristique du bien :</p>
+            <dl>
+                <dt>Superficie :</dt>
+                <dd><?php echo $unbien['surface'] . ' m²'; ?></dd>
+                <dt>Nombre de pièces :</dt>
+                <dd><?php echo $unbien['nbpiece']; ?></dd>
+            </dl>
+            <h3>Description :</h3>
+            <p><?php echo $unbien['description']; ?></p>
+            <button onclick="genPDF();">Générer PDF</button>
         </div>
-        <button onclick="genererBien();">Convert HTML to PDF</button>
-        <script src="../js/html2canvas.min.js"></script>
-        <script src="../js/jspdf.min.js"></script>
-        <script>
-            function genererBien() {
-                //effectivement ce code vient d'Internet
-                var elementHTML = document.getElementById('descri');
-                var imgg = document.getElementById('img_bien');
-                html2canvas(imgg, {
-                    useCORS: true,
-                    onrendered: function (canvas) {
-                        var pdf = new jsPDF('p', 'pt', 'letter');
-                        var pageHeight = 980;
-                        var pageWidth = 900;
-                        for (var i = 0; i <= elementHTML.clientHeight / pageHeight; i++) {
-                            var srcImg = canvas;
-                            var sX = 0;
-                            var sY = pageHeight * i;
-                            var sWidth = pageWidth;
-                            var sHeight = pageHeight;
-                            var dX = 0;
-                            var dY = 0;
-                            var dWidth = pageWidth;
-                            var dHeight = pageHeight;
-                            window.onePageCanvas = document.createElement("canvas");
-                            onePageCanvas.setAttribute('width', pageWidth);
-                            onePageCanvas.setAttribute('height', pageHeight);
-                            var ctx = onePageCanvas.getContext('2d');
-                            ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
-                            var canvasDataURL = onePageCanvas.toDataURL("image/jpeg", 0.90);
-                            var width = onePageCanvas.width;
-                            var height = onePageCanvas.clientHeight;
-                            if (i > 0)
-                                pdf.addPage(612, 864);
-                            pdf.setPage(i + 1);
-                            pdf.addImage(canvasDataURL, 'JPEG', 50, 10);
-                            console.log(canvasDataURL);
-                        }
-                        pdf.save('document_bien.pdf');
-                    }
-                });
-            }
-        </script>
+        
+
     </body>
 </html>
