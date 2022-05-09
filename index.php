d444ba307bcaaa0e828aa23de4a1766e5b76b6cf
Braxasss <antbrasseur1108@gmail.com>
2022-05-09 15:57:27 +0200

    sprint4

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ include_once 'modeles/mesFonctionsAccesBDD.php';
 include_once'inc/enteteindex.inc';
 //include_once 'inc/piedDePage.inc';
 ?>
+
 <p>Page d'accueil</p>
 <h1>Bienvenue sur ce site web !</h1>
 </body>
