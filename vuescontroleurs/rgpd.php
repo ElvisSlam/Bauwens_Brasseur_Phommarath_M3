80484ad23bbce83a8e8862fec56c4576f91af9c7
Braxasss <antbrasseur1108@gmail.com>
2022-10-10 12:01:56 +0200

    politique donnees ajoutée

---
+++ vuescontroleurs/rgpd.php
@@ -0,0 +1,58 @@
+<?php
+include_once '../inc/entete.inc';
+require('../modeles/mesFonctionsAccesBDD.php');
+?>
+<div style="background-color:white; margin-top:50px; margin-right:50px; margin-left:50px; margin-bottom:50px; padding-left:25px; padding-right:25px; padding-top:25px; padding-bottom:25px;">
+<h1 style="text-align:center;">Protection des données</h1></br>
+<h2 style="text-align:center;">Règles de confidentialité</h2></br>
+<p><b>I. Nom et adresse du responsable du traitement</b></p></br>
+<p>Le responsable du traitement au sens de l'Art. 4, par. 7, du Règlement de l'UE N° 2016/679 appelé Règlement général sur la 
+    protection des données (RGPD), ainsi que d'autres lois nationales des États membres concernant la protection des données 
+    et de toutes autres dispositions légales en matière de protection des données, est la société : </p></br>
+<p>France Immobilier</p>
+<p>51 rue des saucisses, BOUDREAUX, FRANCE</p><br>
+<p><b>II. Nom et adresse du délégué à la protection des données</b></p><br>
+<p>Le délégué à la protection des données du responsable du traitement est :</p>
+<p>Elvis Bauwens</p>
+<p>elvisbauwens@mail.com</p><br>
+<p><b>III. Liste des données personnelles collectées</b></p><br>
+<p>La liste des données personnelles collectées est :</p>
+<ul>
+    <li>Adresse email</li>
+    <li>Nom</li>
+    <li>Prénom</li>
+</ul>
+<br>
+<p><b>IV. Finalité de la collecte de ces données</b></p><br>
+<p>Ces données nous servent à vous proposer un service adapté à vos besoins et vous permettre de recevoir notre newletter.</p><br>
+<p><b>V. Base et fondement juridique de la collecte</b></p><br>
+<p>Dans la mesure où nous obtenons un consentement de la personne concernée pour certains traitements des données à caractère personnel, l'Art. 6, par. 1, point a, du RGPD sert de fondement juridique au traitement de données à caractère personnel. </p><br>
+<p>Lors d'un traitement des données à caractère personnel s'avérant indispensable pour l'exécution d'un contrat conclu avec la personne concernée, l'Art. 6, par. 1, point b, du RGPD sert de fondement juridique. Ceci vaut également pour les traitements des données s'avérant indispensables pour l'accomplissement de mesures précontractuelles. </p><br>
+<p>Conformément à l'Art. 6, par. 1, point f), le traitement des données à caractère personnel est également licite lorsqu'il est nécessaire pour préserver les intérêts légitimes du responsable du traitement ou d'un tiers, à moins que ne prévalent les intérêts ou les libertés et droits fondamentaux de la personne concernée requérant la protection des données à caractère personnel, notamment lorsque la personne concernée est un enfant. </p><br>
+<p><b>VI. Durée de conservation des données</b></p><br>
+<p>Les données sont effacées dès qu'elles ne sont plus indispensables au regard des finalités de leur collecte. Concernant l'adresse IP, celle-ci est écourtée, c.-à-d. rendue anonyme, dès que la session respective est terminée. Après cela, les données n'ont plus aucun caractère personnel. L'intégralité des fichiers log est effacée après 50 jours. </p><br>
+<p><b>VII. Liste des tiers ayant accès ou collectant ces données</b></p><br>
+<p>Aucun tiers n'a accès à vos données</p><br>
+<p><b>VIII. Les cookies utilisés dans le site web</b></p><br>
+<p>Avec les cookies techniquement indispensables, les données suivantes sont conservées et transmises :</p><br>
+<ul>
+    <li>Paramètres de langue</li>
+    <li>Informations de login : adresse email, nom, prénom</li>
+    <li>Fréquentation de nos pages</li>
+    <li>Mots-clés recherchés</li>
+</ul><br>
+<p><b>IV. Droits de l'utilisateur vis-à-vis de ces données</b></p><br>
+<p>Si des données à caractère personnel vous concernant sont traitées, vous êtes une « personne concernée » au sens du RGPD et vous disposez alors des droits suivants à l'encontre du responsable du traitement : </p><br>
+<p>Vous avez le droit d’obtenir du responsable la confirmation que des données à caractère personnel vous concernant sont ou ne sont pas traitées par nos services. </p><br>
+<p>Vis-à-vis du responsable, vous avez le droit d’obtenir la rectification ou le complément de données à caractère personnel vous concernant dans la mesure où lesdites données sont inexactes ou incomplètes. Le responsable est tenu de procéder à la rectification dans les meilleurs délais. </p><br>
+<p>Lorsque le traitement des données à caractère personnel vous concernant a été limité, ces données ne peuvent – à l’exception de la conservation – être traitées qu’avec votre consentement, ou pour faire valoir, exercer ou défendre des droits en justice, ou pour la protection des droits d’une autre personne physique ou morale, ou encore pour des motifs d’intérêt public importants de l’Union ou d’un État membre. </p><br>
+<p>Vous pouvez obtenir du responsable l’effacement, dans les meilleurs délais, de données à caractère personnel vous concernant, et le responsable a l’obligation d’effacer ces données dans les meilleurs délais</p><br>
+<p>Lorsque le responsable a rendu publiques les données à caractère personnel vous concernant et qu’il est tenu de les effacer conformément à l’Art. 17, par. 1, du RGPD, le responsable – compte tenu des technologies disponibles et des coûts de mise en œuvre – prend des mesures appropriées (y compris d’ordre technique) pour informer les tiers responsables traitant ces données à caractère personnel que vous avez demandé, en tant que personne concernée, l’effacement par ces responsables de tous liens vers ces données à caractère personnel et de toutes copies ou reproductions de celles-ci. </p><br>
+<p>Si vous avez fait valoir votre droit à la rectification ou à l’effacement de données ou à la limitation du traitement vis-à-vis du responsable, ce dernier est alors tenu de notifier cette rectification ou cet effacement de données ou cette limitation du traitement à chaque destinataire auquel les données à caractère personnel vous concernant ont été communiquées, à moins que ceci se révèle impossible ou exige des efforts disproportionnés. </p><br>
+<p>Vous avez le droit de recevoir les données à caractère personnel vous concernant, et que vous avez fournies au responsable, dans un format structuré, couramment utilisé et lisible par machine.</p><br>
+<p>Vous avez le droit de vous opposer à tout moment, pour des raisons tenant de votre situation particulière, à un traitement des données à caractère personnel vous concernant fondé sur l’Art. 6, par. 1, point e ou f, du RGPD ; ceci s’applique également à un profilage fondé sur ces dispositions. </p><br>
+<p>Vous avez le droit de retirer à tout moment votre consentement en matière de protection des données. Le retrait du consentement ne compromet pas la licéité du traitement fondé sur le consentement effectué avant ce retrait. </p><br>
+<p>Vous avez le droit de ne pas faire l'objet d'une décision fondée exclusivement sur un traitement automatisé – y compris le profilage – produisant des effets juridiques vous concernant ou vous affectant de manière significative de façon similaire.</p><br>
+<p>Sans préjudice de tout autre recours administratif ou juridictionnel, vous avez le droit d'introduire une réclamation auprès d'une autorité de contrôle, en particulier dans l'État membre dans lequel se trouve votre résidence habituelle, votre lieu de travail ou le lieu où la violation aurait été commise, si vous considérez que le traitement de données à caractère personnel vous concernant constitue une violation du RGPD. </p><br>
+<p>Nous nous réservons le droit de modifier de temps à autre les présentes directives en matière de protection des données. La version respectivement en vigueur est consultable sur notre site web. Au cas où une modification restreindrait de manière significative les droits des utilisateurs inscrits, nous en informerons alors ceux-ci. Du reste, la déclaration de protection des données actuellement consultable est respectivement applicable aux utilisateurs de notre site web. </p>
+</div>
